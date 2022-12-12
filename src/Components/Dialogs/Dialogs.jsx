a00c51016dbded2506caf6e13507385a73ba3cb2
DmitriyHrushko <dmitriyhryshko@gmail.com>
2022-12-12 16:11:21 +0200

    added Dialogs/Fixed Navlinks

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
-	return <div>Dialogs</div>;
+	return (
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog + ' ' + s.active}>
+					<NavLink to="/dialogs/1">First</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink to="/dialogs/2">Second</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink to="/dialogs/3">Third</NavLink>
+				</div>
+				<div className={s.dialog}>
+					<NavLink to="/dialogs/4">Fourth</NavLink>
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>hi</div>
+				<div className={s.message}>hi</div>
+				<div className={s.message}>hi</div>
+			</div>
+		</div>
+	);
 };
 
 export default Dialogs;
