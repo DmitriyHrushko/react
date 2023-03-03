9543b3b06f2bbe0b7490891180cb81733397e2d2
DmitriyHrushko <dmitriyhryshko@gmail.com>
2023-03-03 10:42:40 +0200

    action/dispatch in store

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -5,12 +5,14 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 	let postsElement = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
 	let newPostElement = React.createRef();
+
 	let addPost = () => {
-		props.addPost();
+		props.dispatch({ type: 'ADD-POST' });
 	};
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		props.updateNewPostText(text);
+		let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+		props.dispatch(action);
 	};
 	return (
 		<div className={s.postsBlock}>
