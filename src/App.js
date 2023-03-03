2d4f8414535377dabafbba594a733e6be290e6c0
DmitriyHrushko <dmitriyhryshko@gmail.com>
2023-03-03 16:35:38 +0200

    messages added

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ const App = (props) => {
 				<Navbar />
 				<div className="app-wrapper-content">
 					<Routes>
-						<Route path="/dialogs" element={<Dialogs
-							state={props.state.dialogsPage} />} />
-						<Route path="/profile" element={<Profile
-							profilePage={props.state.profilePage}
-							dispatch={props.dispatch} />} />
+						<Route path="/dialogs" element=
+							{<Dialogs store={props.store} />} />
+						<Route path="/profile" element=
+							{<Profile
+								profilePage={props.state.profilePage}
+								dispatch={props.dispatch} />} />
 					</Routes>
 				</div>
 			</div>
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewActionPostCreator } from '../../../redux/state';
 
 const MyPosts = (props) => {
 	let postsElement = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
 	let newPostElement = React.createRef();
 
 	let addPost = () => {
-		props.dispatch({ type: 'ADD-POST' });
+		props.dispatch(addPostActionCreator());
 	};
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+		let action = updateNewActionPostCreator(text);
 		props.dispatch(action);
 	};
 	return (
