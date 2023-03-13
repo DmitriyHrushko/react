822bb4d0f73ef0967c30be2097c24d466b25412a
DmitriyHrushko <dmitriyhryshko@gmail.com>
2023-03-13 19:31:25 +0200

    cookie/login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
 import DialogsContainer from './Components/Dialogs/DialogsContainer';
-import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UsersContainer from './Components/Users/UsersContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
+import HeaderContainer from './Components/Header/HeaderContainer';
 
 const App = (props) => {
 	return (
 		<BrowserRouter>
 			<div className="app-wrapper">
-				<Header />
+				<HeaderContainer />
 				<Navbar />
 				<div className="app-wrapper-content">
 					<Routes>
 						<Route path="/dialogs" element=
 							{<DialogsContainer />} />
+						<Route path="/profile/:userId" element=
+							{<ProfileContainer />} />
 						<Route path="/profile/*" element=
 							{<ProfileContainer />} />
 						<Route path="/users" element=
